ade387ae5b74de397cb34b85dc7e7597abe8bb89
hgg23 <hannah.gillespie23@imperial.ac.uk>
2023-11-28 13:28:44 +0000

    added database connection stuff

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -5,10 +5,19 @@ import configparser
 dirname = os.getcwd()
 config = configparser.ConfigParser()
 config.read(os.path.join(dirname, 'tests/assets/mock_dbtool.ini'))
+print(config.sections())
 
 conn = mock_db.connect(**config['connection'])
 curs = conn.cursor()
 
-curs.execute(config['sample_query'])
+#curs.execute(config['delete_test_schema']['delete_test_database'])
+curs.execute(config['create_test_schema']['test_database'])
+#curs.execute(config['delete_charity_table']['delete_charities'])
+curs.execute(config['create_charity_table']['charity_table'])
+curs.execute(config['insert_into_charity_table']['insert_svp'])
+curs.execute(config['sample_charity_query']['all_charities'])
+rec=curs.fetchall()
+print(rec)
 
+#conn.commit() - will use only for the live database
 conn.close()
