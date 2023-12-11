b24984f82ffdda0b7e06921795927f77e1fe3228
Hannah Gillespie <hannahgillespie@dyn3253-61.wlan.ic.ac.uk>
2023-12-11 14:55:46 +0000

    trying to figure out pytest

--- tests/test_database.py
+++ tests/test_database.py
@@ -287,3 +287,104 @@ def test_inserting_multiple_items_into_message_table(connect_to_database, create
     )
     rec = curs.fetchall()
     assert(len(rec) == 2)
+
+def test_flexible_query(connect_to_database, create_schedule_table):
+    (curs, config) = connect_to_database
+    # Set up charity table with multiple entities
+    curs.execute(config['insert_into']['charity_table'].replace(
+        '@schema_name@', SCHEMA_NAME), ['SVP', 123]
+    )
+    curs.execute(config['insert_into']['charity_table'].replace(
+        '@schema_name@', SCHEMA_NAME), ['Feed', 312]
+    )
+
+    # Insert four entries into schedule table and assert presence
+    curs.execute(config['insert_into']['schedule_table'].replace(
+        '@schema_name@', SCHEMA_NAME), [123, 'Monday', 'Afternoon', 'Chelsea']
+    )
+    curs.execute(config['insert_into']['schedule_table'].replace(
+        '@schema_name@', SCHEMA_NAME), [123, 'Wednesday', 'Morning', 'Camden']
+    )
+    curs.execute(config['insert_into']['schedule_table'].replace(
+        '@schema_name@', SCHEMA_NAME), [312, 'Wednesday', 'Morning', 'Southwark']
+    )
+    curs.execute(config['insert_into']['schedule_table'].replace(
+        '@schema_name@', SCHEMA_NAME), [312, 'Tuesday', 'Evening', 'Camden']
+    )
+    curs.execute(config['insert_into']['schedule_table'].replace(
+        '@schema_name@', SCHEMA_NAME), [123, 'Wednesday', 'Afternoon', 'Ealing']
+    )
+    curs.execute(config['query']['select_all'].replace(
+        '@schema_name@', SCHEMA_NAME).replace(
+        '@table_name@', 'schedule'
+    ))
+    rec = curs.fetchall()
+    assert(len(rec) == 5)
+
+    criteria_to_add = []
+    items_to_input = []
+
+    day = None
+    time = None
+    location = None
+    charity = None
+
+    if day:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.day=%s')
+        items_to_input.append(day)
+    if time:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.time=%s')
+        items_to_input.append(time)
+    if location:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.location=%s')
+        items_to_input.append(location)
+    if charity:
+        criteria_to_add.append(SCHEMA_NAME + '.charity.name=%s')
+        items_to_input.append(charity)
+
+    criteria = ''
+    for item in criteria_to_add:
+        if criteria == '':
+            criteria += 'WHERE ' + item
+        else:
+            criteria += ' AND ' + item
+
+    curs.execute(config['query']['select_query_experiment'].replace(
+            '@schema_name@', SCHEMA_NAME).replace('@criteria@', criteria), items_to_input)
+    rec = curs.fetchall()
+    print(rec)
+    assert(len(rec) == 5)
+
+    criteria_to_add = []
+    items_to_input = []
+
+    day = None
+    time = "Afternoon"
+    location = None
+    charity = "SVP"
+
+    if day:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.day=%s')
+        items_to_input.append(day)
+    if time:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.time=%s')
+        items_to_input.append(time)
+    if location:
+        criteria_to_add.append(SCHEMA_NAME + '.schedule.location=%s')
+        items_to_input.append(location)
+    if charity:
+        criteria_to_add.append(SCHEMA_NAME + '.charity.name=%s')
+        items_to_input.append(charity)
+
+    criteria = ''
+    for item in criteria_to_add:
+        if criteria == '':
+            criteria += 'WHERE ' + item
+        else:
+            criteria += ' AND ' + item
+
+    curs.execute(config['query']['select_query_experiment'].replace(
+            '@schema_name@', SCHEMA_NAME).replace('@criteria@', criteria), items_to_input)
+    rec = curs.fetchall()
+    print(rec)
+    assert(len(rec) == 2)
